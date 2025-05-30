d6ee8647a7947c8790a49647561c93a73b301822
Hassanein nazha <Nazha.hassanein@gmail.com>
2025-05-30 14:29:13 +0300

    ok

---
+++ script.py
@@ -0,0 +1,47 @@
+# number=10
+# num=11.5
+# number=num
+# print(number)
+# input("Name: ")
+# input("Age: ")
+# name=input("Name: ")
+# age=input("Age: ")
+# print("Hello", name ," you are ", age ,"years old")
+# number1=10
+# number2=3
+# number3=15
+# result=(number1+number2)*number3
+# print(result)
+
+# number1=input("number1: ")
+# number2=input("number2: ")
+# number1=float(number1)
+# number2=float(number2)
+# result=number1*number2
+# print(result)
+# str="hassanein nazha"
+# str_upper=str.upper()
+# print(str_upper)
+   
+# str_lower=str.lower()
+# print(str_lower)
+
+# str_cap=str.capitalize()
+# print(str_cap)
+
+# str_len=len(str)
+# print("count of chars: ", str_len)
+
+# count=str.lower().count("n",5)
+# print(count)
+
+number1=input("Number1: ")
+number2=input("Number2: ")
+
+number1=int(number1)
+number2=int(number2)
+
+print("type of number1: ", type(number1))
+print("type of number2: ", type(number2))
+# result=number1*number2
+# print(result)
